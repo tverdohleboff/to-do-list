bd586fbd8b5e2ee46e5a1b51b4dffb0759665740
Alex <tverdohleboff@gmail.com>
2023-02-18 18:26:24 +0400

    Add red color for expired task

--- src/Task.js
+++ src/Task.js
@@ -25,8 +25,12 @@ function Task(props) {
   function handleEditButton(){
     setEditTaskId(id);
   }
+ 
+  const isExpired = new Date(date).getTime() < new Date().getTime();
 
   const className = isChecked ? 'Task Task_Checked' : 'Task';
+  const dateClassName = isExpired ? 'Date redText' : 'Date';
+  const nameClassName = isExpired ? 'Name redText' : 'Name';
 
   return (
     <div className={className}>
@@ -35,11 +39,11 @@ function Task(props) {
         onChange={handleCheckboxChange} 
         checked={isChecked}
       />
-      <div className='Date'>{new Date(date).toLocaleString('ru-RU', {
+      <div className={dateClassName}>{new Date(date).toLocaleString('ru-RU', {
         dateStyle: 'short',
         timeStyle: 'short'
       })}</div>
-      <div className='Name'>{name}</div>
+      <div className={nameClassName}>{name}</div>
       <button 
         className='editTask actionWithTask' 
         type='button'
